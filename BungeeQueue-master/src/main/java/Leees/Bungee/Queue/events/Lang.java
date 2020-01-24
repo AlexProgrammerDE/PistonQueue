9966df6b4a33b85718d3bf59e939156db439844b
Leeeunderscore <43287584+Leeeunderscore@users.noreply.github.com>
2020-01-24 14:02:28 -0700

    release 1.0.2
    release 1.0.2 added queue bypass permission

---
+++ BungeeQueue-master/src/main/java/Leees/Bungee/Queue/events/Lang.java
@@ -0,0 +1,11 @@
+package Leees.Bungee.Queue.events;
+
+/**
+ * Lang
+ */
+public class Lang {
+
+    public static String SEND_TO_LIMBO, CURRENT_LIMBO_POSITION, LEFT_LIMBO_JOIN_SERVER, QUEUE_OVERRIDE_PERMISSION,
+            GLOBAL_SLOTS_PERMISSION, SUPER_SLOTS_OVERRIDE_PERMISSION;
+    public static int GLOBAL_SLOTS, SUPER_SLOTS;
+}
