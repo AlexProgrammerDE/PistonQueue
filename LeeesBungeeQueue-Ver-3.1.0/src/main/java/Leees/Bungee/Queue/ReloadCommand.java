ec3bf59f86d2a657c1c8bdfce41a161ad06198bb
Leeeunderscore <43287584+Leeeunderscore@users.noreply.github.com>
2020-06-06 19:55:48 -0600

    3.1.0
    Finally fixed the yellow warning in console that ment nothing and a reminder if you have any suggestions for features i should add to join the support discord and tell me them and also to please leave a review if you like this plugin or have an issue with it

---
+++ LeeesBungeeQueue-Ver-3.1.0/src/main/java/Leees/Bungee/Queue/ReloadCommand.java
@@ -0,0 +1,58 @@
+package Leees.Bungee.Queue;
+
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.plugin.Command;
+
+public class ReloadCommand extends Command {
+
+    public ReloadCommand() {
+        super("lbq");
+    }
+
+    @Override
+    public void execute(CommandSender sender, String[] args) {
+        if (args.length < 1 || args[0].equalsIgnoreCase("help"))  {
+            sender.sendMessage(ChatColor.DARK_BLUE + "----------------");
+            sender.sendMessage(ChatColor.GOLD + "LeeesBungeeQueue");
+            sender.sendMessage(ChatColor.GOLD + "/lbq help");
+            sender.sendMessage(ChatColor.GOLD + "/lbq reload");
+            sender.sendMessage(ChatColor.GOLD + "/lbq version");
+            sender.sendMessage(ChatColor.GOLD + "/lbq stats");
+            sender.sendMessage(ChatColor.DARK_BLUE + "----------------");
+            return;
+        }
+        if (args[0].equalsIgnoreCase("version")) {
+            sender.sendMessage(ChatColor.DARK_BLUE + "----------------");
+            sender.sendMessage(ChatColor.GOLD + "LeeesBungeeQueue");
+            sender.sendMessage(ChatColor.GOLD + "Version 3.1.0 by");
+            sender.sendMessage(ChatColor.GOLD + "Nate Legault");
+            sender.sendMessage(ChatColor.DARK_BLUE + "----------------");
+            return;
+        }
+        if (args[0].equalsIgnoreCase("stats")) {
+            sender.sendMessage(ChatColor.DARK_BLUE + "----------------");
+            sender.sendMessage(ChatColor.GOLD + "Queue stats");
+            sender.sendMessage(ChatColor.GOLD + "Priority: " + ChatColor.BOLD + QueuePlugin.priorityqueue.size());
+            sender.sendMessage(ChatColor.GOLD + "Regular: " + ChatColor.BOLD + QueuePlugin.regularqueue.size());
+            sender.sendMessage(ChatColor.DARK_BLUE + "----------------");
+            return;
+        }
+        if (sender.hasPermission(Lang.ADMINPERMISSION)) {
+            if (args[0].equalsIgnoreCase("reload")) {
+                QueuePlugin.getInstance().processConfig();
+                sender.sendMessage(ChatColor.DARK_BLUE + "----------------");
+                sender.sendMessage(ChatColor.GOLD + "LeeesBungeeQueue");
+                sender.sendMessage(ChatColor.GREEN + "Config reloaded");
+                sender.sendMessage(ChatColor.DARK_BLUE + "----------------");
+                return;
+            }
+            } else {
+            sender.sendMessage(ChatColor.DARK_BLUE + "----------------");
+            sender.sendMessage(ChatColor.GOLD + "LeeesBungeeQueue");
+            sender.sendMessage(ChatColor.RED + "You do not");
+            sender.sendMessage(ChatColor.RED + "have permission");
+            sender.sendMessage(ChatColor.DARK_BLUE + "----------------");
+            }
+    }
+}
