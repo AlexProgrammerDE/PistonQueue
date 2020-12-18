46e850638ec17963601edd234ac66300ca902857
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2020-12-18 18:49:19 +0100

    Final bit ;)

--- src/main/java/Xera/Bungee/Queue/Bungee/ChatUtils.java
+++ src/main/java/Xera/Bungee/Queue/Bungee/ChatUtils.java
@@ -4,7 +4,7 @@ import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.TextComponent;
 
-public class ChatUtils {
+public final class ChatUtils {
     public static String parseToString(String str) {
         return ChatColor.translateAlternateColorCodes('&', XeraBungeeQueue.parseText(str));
     }
