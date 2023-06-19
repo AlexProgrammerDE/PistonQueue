d18dd237c1e498d0f1797975b684a1acc94ad675
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2023-06-19 12:46:37 +0200

    Fix buildSrc java version

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,6 +11,3 @@ dependencies {
     implementation("gradle.plugin.com.github.johnrengelman:shadow:8.0.0")
     implementation("net.kyori:indra-common:3.1.1")
 }
-
-java.targetCompatibility = JavaVersion.VERSION_1_8
-java.sourceCompatibility = JavaVersion.VERSION_1_8
