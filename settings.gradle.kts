e7d7f707f760c6a8aaed104af85ce17567c3b67d
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2025-05-04 20:02:15 +0200

    fix: add toolchain plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,6 +14,10 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
+}
+
 dependencyResolutionManagement {
     @Suppress("UnstableApiUsage")
     repositories {
