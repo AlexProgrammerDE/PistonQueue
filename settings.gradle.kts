6b2b7a89b0f3f7570de7e0c2c556b4d108501e2c
Alex <40795980+AlexProgrammerDE@users.noreply.github.com>
2025-10-22 14:47:42 +0200

    Remove unnecessary plugins from settings.gradle.kts
    Removed several plugin declarations from pluginManagement.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,13 +4,6 @@ pluginManagement {
     repositories {
         gradlePluginPortal()
     }
-    plugins {
-        id("com.github.johnrengelman.shadow") version "8.1.1"
-        id("net.kyori.indra") version "4.0.0"
-        id("net.kyori.indra.git") version "4.0.0"
-        id("net.kyori.indra.publishing") version "4.0.0"
-        id("net.kyori.blossom") version "2.2.0"
-    }
 }
 
 plugins {
