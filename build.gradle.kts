fe73d013fb7dd71164674164246edf210d806f76
Alex <40795980+AlexProgrammerDE@users.noreply.github.com>
2026-01-30 09:49:27 +0100

    Remove outputVersion task from build.gradle.kts
    Removed the 'outputVersion' task from the build script.

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,9 +11,3 @@ allprojects {
     version = property("maven_version")!!
     description = "Best queue plugin out there!"
 }
-
-tasks.register("outputVersion") {
-    doLast {
-        println(project.version)
-    }
-}
